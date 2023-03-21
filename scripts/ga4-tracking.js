7575c28f080336584886fcde1cec57e255e1c70d
Jirayu Tancharoen <benzene@Jirayus-MacBook-Air.local>
2023-03-21 18:46:38 +0700

    fix gtag

---
+++ scripts/ga4-tracking.js
@@ -0,0 +1,10 @@
+// <!-- Google tag (gtag.js) --> 
+var script = document.createElement('script');
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-2M5BYZBY34';
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-2M5BYZBY34');
